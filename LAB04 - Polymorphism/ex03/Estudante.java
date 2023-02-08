d86802b83bf6d005c88888710afed6fa0859af8a
klariaAndradeMartins <Klariaandrade@gmail.com>
2023-02-07 23:00:06 -0300

    Lab04 - ex03

---
+++ LAB04 - Polymorphism/ex03/Estudante.java
@@ -0,0 +1,29 @@
+public abstract class Estudante {
+    private String nome;
+    private String endereco;
+
+    public Estudante(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public void print() {
+        System.out.println("Estudante{nome='" + this.nome + "', endereco='" + this.endereco + "'}");
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ LAB04 - Polymorphism/ex03/Universidade.java
@@ -0,0 +1,10 @@
+public class Universidade extends Estudante{
+
+    protected String Universidade;
+
+    protected String dtFundação;
+
+    public Universidade(String nome, String endereco) {
+        super(nome, endereco);
+    }
+}
